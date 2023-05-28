b4e1f2f5c6977b7df8d27aade6b7dac294433ec3
maxdrv <maxdrv.stud@gmail.com>
2023-05-28 20:47:34 +0500

    store to file console program example

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -4,40 +4,37 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final String FILE_TO_STORE = "storage.txt";
-
     public static void main(String[] args) throws IOException {
-        List<String> storedLines;
-
-        File file = new File(FILE_TO_STORE);
-        if (file.exists()) {
-            storedLines = Files.readAllLines(file.toPath());
-        } else {
+        File file = new File("storage.txt");
+        if (!file.exists()) {
             file.createNewFile();
-            storedLines = new ArrayList<>();
         }
 
         while (true) {
             String line = new Scanner(System.in).nextLine();
             if (line.startsWith("add ")) {
-                String substring = line.substring(4);
-                storedLines.add(substring);
+                String substring = line.substring(4);  // убрал первые 4 символа
 
                 // add to file
                 String textToAdd = substring + "\n";
                 Files.write(file.toPath(), textToAdd.getBytes(), StandardOpenOption.APPEND);
 
             } else if (Objects.equals(line, "print")) {
-                for (String storedLine : storedLines) {
+                for (String storedLine : Files.readAllLines(file.toPath())) {
                     System.out.println(storedLine);
                 }
+            } else if (line.startsWith("delete ")) {
+                String substring = line.substring(7);
+                int idx = Integer.parseInt(substring);
+                List<String> lines = Files.readAllLines(file.toPath());  // читаю все линии
+                lines.remove(idx);  // удаляю по индексу
+                Files.write(file.toPath(), lines);  // записываю все оставшиеся линии в файл
             } else if (Objects.equals(line, "exit")) {
                 break;
             } else {
