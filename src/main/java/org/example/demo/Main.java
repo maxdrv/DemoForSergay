3446a34ecd8d480c0611e01225f9dcf43fea4343
maxdrv <maxdrv.stud@gmail.com>
2023-05-28 20:56:52 +0500

    line -> input

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -19,25 +19,26 @@ public class Main {
         Path path = file.toPath();
 
         while (true) {
-            String line = new Scanner(System.in).nextLine();
-            if (line.startsWith("add ")) {
-                String substring = line.substring(4);  // убрал первые 4 символа
+            String input = new Scanner(System.in).nextLine();
+            if (input.startsWith("add ")) {
+                String substring = input.substring(4);  // убрал первые 4 символа
 
                 // add to file
                 String textToAdd = substring + "\n";
                 Files.write(path, textToAdd.getBytes(), StandardOpenOption.APPEND);
 
-            } else if (Objects.equals(line, "print")) {
-                for (String storedLine : Files.readAllLines(path)) {
-                    System.out.println(storedLine);
+            } else if (Objects.equals(input, "print")) {
+                List<String> lines = Files.readAllLines(path);
+                for (String line : lines) {
+                    System.out.println(line);
                 }
-            } else if (line.startsWith("delete ")) {
-                String substring = line.substring(7);
+            } else if (input.startsWith("delete ")) {
+                String substring = input.substring(7);
                 int idx = Integer.parseInt(substring);
                 List<String> lines = Files.readAllLines(path);  // читаю все линии
-                lines.remove(idx);  // удаляю по индексу
-                Files.write(path, lines);  // записываю все оставшиеся линии в файл
-            } else if (Objects.equals(line, "exit")) {
+                lines.remove(idx);                              // удаляю по индексу
+                Files.write(path, lines);                       // записываю все оставшиеся линии в файл
+            } else if (Objects.equals(input, "exit")) {
                 break;
             } else {
                 System.out.println("only add and exit is valid commands");
