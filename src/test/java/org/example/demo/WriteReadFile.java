257d259d6e2788eb29c5c5412613c73a98e4ffb9
maxdrv <maxdrv.stud@gmail.com>
2023-11-10 20:40:24 +0100

    Examples for read and write to/from file

--- src/test/java/org/example/demo/WriteReadFile.java
+++ src/test/java/org/example/demo/WriteReadFile.java
@@ -1,5 +1,6 @@
 package org.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -12,20 +13,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WriteReadFile {
 
+    @DisplayName("Вставить содержимое в файл с заменой")
     @Test
-    void writeFile() throws IOException {
+    void insertContentIntoFile() throws IOException {
         String text = "any text to write";
         File file = new File("file.txt");  // файл в текущей папка, где запускается программа
-        Files.write(file.toPath(), text.getBytes());
+        Files.write(file.toPath(), text.getBytes());  // полностью затираем старое содержимое файла, заменяем на новое содержимое
+    }
 
-        Files.write(file.toPath(), "\n".getBytes(), StandardOpenOption.APPEND);  // перевод на следующую строку
-
+    @DisplayName("Дописать в конец файла")
+    @Test
+    void appendNewLineToFile() throws IOException {
+        File file = new File("file.txt");
+        Files.write(file.toPath(), "\n".getBytes(), StandardOpenOption.APPEND);  // добавили перенос строки
         String secondLine = "second line";
         Files.write(file.toPath(), secondLine.getBytes(), StandardOpenOption.APPEND);
     }
 
+    @DisplayName("Прочитать файл построчно")
     @Test
-    void readFile() throws IOException {
+    void readAllLinesFromFile() throws IOException {
         File file = new File("file.txt");
         List<String> lines = Files.readAllLines(file.toPath());
 
@@ -33,5 +40,13 @@ public class WriteReadFile {
                 .containsExactly("any text to write", "second line");
     }
 
+    @DisplayName("Прочитать файл целиком")
+    @Test
+    void readWholeFile() throws IOException {
+        File file = new File("file.txt");
+        String content = Files.readString(file.toPath());
+        assertThat(content)
+                .contains("any text to write\nsecond line");
+    }
 
 }
